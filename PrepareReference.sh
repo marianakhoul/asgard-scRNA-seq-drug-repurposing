dc724eb97e8b8063368010a2ba2b117eb9f17ddb
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2023-05-09 18:23:30 -0400

    Update PrepareReference.sh

--- PrepareReference.sh
+++ PrepareReference.sh
@@ -3,8 +3,10 @@
 #SBATCH -p short
 #SBATCH -t 12:00:00
 #SBATCH --mem=8G
+#SBATCH -o %j.out 
+#SBATCH -e %j.err
 #SBATCH --mail-type=ALL
-#SBATCH –-mail-user=ssalem5@bwh.harvard.edu
+#SBATCH --mail-user=ssalem5@bwh.harvard.edu
 
 module load gcc/9.2.0 R/4.2.1
 
