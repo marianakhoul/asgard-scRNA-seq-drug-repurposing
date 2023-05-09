e26e920be50f019c092ba2fb588aa97bd158a1b7
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2023-05-09 18:17:37 -0400

    Update PrepareReference.R

--- PrepareReference.R
+++ PrepareReference.R
@@ -2,14 +2,12 @@
 
 library('Asgard')
 
-#Please replace Your_local_path with your real local folder
-#Also, the names of these files may be different after unzipping, change them when unzipped.
-PrepareReference(cell.info="GSE70138_Broad_LINCS_cell_info_2017-04-28.txt",
-                 gene.info="GSE70138_Broad_LINCS_gene_info_2017-03-06.txt",
-                 GSE70138.sig.info = "GSE70138_Broad_LINCS_sig_info_2017-03-06.txt",
-                 GSE92742.sig.info = "GSE92742_Broad_LINCS_sig_info.txt",
-                 GSE70138.gctx = "GSE70138_Broad_LINCS_Level5_COMPZ_n118050x12328_2017-03-06.gctx",
-                 GSE92742.gctx = "GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx",
-                 Output.Dir = "DrugReference/")# change this directory and the above file locations
+PrepareReference(cell.info="/home/sas1782/asgard-scRNA-seq-drug-repurposing/GEOProfiles/GSE70138_Broad_LINCS_cell_info_2017-04-28.txt",
+                 gene.info="/home/sas1782/asgard-scRNA-seq-drug-repurposing/GEOProfiles/GSE70138_Broad_LINCS_gene_info_2017-03-06.txt",
+                 GSE70138.sig.info = "/home/sas1782/asgard-scRNA-seq-drug-repurposing/GEOProfiles/GSE70138_Broad_LINCS_sig_info_2017-03-06.txt",
+                 GSE92742.sig.info = "/home/sas1782/asgard-scRNA-seq-drug-repurposing/GEOProfiles/GSE92742_Broad_LINCS_sig_info.txt",
+                 GSE70138.gctx = "/home/sas1782/asgard-scRNA-seq-drug-repurposing/GEOProfiles/GSE70138_Broad_LINCS_Level5_COMPZ_n118050x12328_2017-03-06.gctx",
+                 GSE92742.gctx = "/home/sas1782/asgard-scRNA-seq-drug-repurposing/GEOProfiles/GSE92742_Broad_LINCS_Level5_COMPZ.MODZ_n473647x12328.gctx",
+                 Output.Dir = "/home/sas1782/asgard-scRNA-seq-drug-repurposing/DrugReference/")# change this directory and the above file locations
 
 #Please note that it takes more than one hour to produce drug references in a standard computer with RAM>64GB.
