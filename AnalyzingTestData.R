fb70a1b167c014dd09de484e809cd067f2c6854e
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2023-05-09 20:08:00 -0400

    Update AnalyzingTestData.R

--- AnalyzingTestData.R
+++ AnalyzingTestData.R
@@ -9,7 +9,7 @@ library(Hmisc)
 ## Step 1: Load single-cell RNA-seq data
 #Load normal sample Ind5 from GSE113197 dataset
 celltype<-read.table(file="https://raw.githubusercontent.com/lanagarmire/Single-cell-drug-repositioning/master/Normal_celltype.txt",header = T,check.names=FALSE)
-data<-read.table(file="GSM3099847_Ind5_Expression_Matrix.txt",header = T,check.names=FALSE)
+data<-read.table(file="/home/sas1782/asgard-scRNA-seq-drug-repurposing/testing_data/GSE113197/GSM3099847_Ind5_Expression_Matrix.txt",header = T,check.names=FALSE)
 row.names(data)<-data[,1]
 data<-data[,-1]
 celltype2<-subset(celltype,sample=="Ind5" & celltype %in% c("Luminal_L2_epithelial_cells","Luminal_L1.1_epithelial_cells", "Luminal_L1.2_epithelial_cells", "Basal_epithelial_cells"))
@@ -18,7 +18,7 @@ data<-data[,common]
 Epithelial2 <- CreateSeuratObject(counts = data, project = "Epithelial", min.cells = 3, min.features = 200,meta.data=data.frame(celltype2,cell=colnames(data),type="Normal"))
 
 #Load normal sample Ind6 from GSE113197 dataset
-data<-read.table(file="GSM3099848_Ind6_Expression_Matrix.txt",header = T,check.names=FALSE)
+data<-read.table(file="/home/sas1782/asgard-scRNA-seq-drug-repurposing/testing_data/GSE113197/GSM3099848_Ind6_Expression_Matrix.txt",header = T,check.names=FALSE)
 row.names(data)<-data[,1]
 data<-data[,-1]
 celltype3<-subset(celltype,sample=="Ind6" & celltype %in% c("Luminal_L2_epithelial_cells","Luminal_L1.1_epithelial_cells", "Luminal_L1.2_epithelial_cells", "Basal_epithelial_cells"))
@@ -27,7 +27,7 @@ data<-data[,common]
 Epithelial3 <- CreateSeuratObject(counts = data, project = "Epithelial", min.cells = 3, min.features = 200,meta.data=data.frame(celltype3,cell=colnames(data),type="Normal"))
 
 #Load normal sample Ind7 from GSE113197 dataset
-data<-read.table(file="GSM3099849_Ind7_Expression_Matrix.txt",header = T,check.names=FALSE)
+data<-read.table(file="/home/sas1782/asgard-scRNA-seq-drug-repurposing/testing_data/GSE113197/GSM3099849_Ind7_Expression_Matrix.txt",header = T,check.names=FALSE)
 row.names(data)<-data[,1]
 data<-data[,-1]
 celltype4<-subset(celltype,sample=="Ind7" & celltype %in% c("Luminal_L2_epithelial_cells","Luminal_L1.1_epithelial_cells", "Luminal_L1.2_epithelial_cells", "Basal_epithelial_cells"))
