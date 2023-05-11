e93c859f6370cf8bba7c40fdb60d4c091e5aed8d
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2023-05-11 11:52:26 -0400

    Update AnalyzingTestData.sh

--- AnalyzingTestData.sh
+++ AnalyzingTestData.sh
@@ -2,10 +2,10 @@
 
 #SBATCH -p short
 #SBATCH -t 12:00:00
-#SBATCH --mem=8G
+#SBATCH --mem=45G
 #SBATCH -o %j.out 
 #SBATCH -e %j.err
 #SBATCH --mail-type=ALL
 
 module load gcc/9.2.0 R/4.2.1
-./AnalyzingTestData.R
+./Step3to6.R
