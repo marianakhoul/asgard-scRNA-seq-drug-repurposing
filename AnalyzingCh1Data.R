458703ccc9a2b103934086e998d8b381f2b8ba45
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2023-05-11 14:30:56 -0400

    Create AnalyzingCh1Data.R

---
+++ AnalyzingCh1Data.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+# Load libraries
+library('Seurat')
+library('Asgard')
+library('Hmisc')
+
+load("/home/sas1782/asgard-scRNA-seq-drug-repurposing/data/FinalQC_ch1newT_nodoublets.RData")
+load("/home/sas1782/asgard-scRNA-seq-drug-repurposing/data/FinalQC_ch1n_nodoublets.RData")
+
