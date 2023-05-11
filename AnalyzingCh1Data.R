5cdc7142f21f9fe1f92343da303a27eb027a7852
Maria Nakhoul <31465978+marianakhoul@users.noreply.github.com>
2023-05-11 14:39:08 -0400

    Update AnalyzingCh1Data.R

--- AnalyzingCh1Data.R
+++ AnalyzingCh1Data.R
@@ -5,6 +5,11 @@ library('Seurat')
 library('Asgard')
 library('Hmisc')
 
+## Step 1: Load single-cell RNA-seq data
 load("/home/sas1782/asgard-scRNA-seq-drug-repurposing/data/FinalQC_ch1newT_nodoublets.RData")
 load("/home/sas1782/asgard-scRNA-seq-drug-repurposing/data/FinalQC_ch1n_nodoublets.RData")
 
+## Step 2: Single-cell alignment
+SC.list<-list(TNBC.PDX2=TNBC.PDX2,TNBC.PDX3=TNBC.PDX3,Epithelial2=Epithelial2,Epithelial3=Epithelial3,Epithelial4=Epithelial4)
+CellCycle=FALSE #Set it TRUE if you want to do Cell Cycle Regression
+anchor.features=2000
